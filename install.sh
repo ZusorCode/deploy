c2bdf91ffc1c684419ff193ff2cc4f7a7fb1797a
Tobias <tobias.d.messner@gmail.com>
2018-04-10 14:19:11 -0600

    reg

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install nginx python3 python3-pip software-properties-common
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get update
-sudo apt-get install python-certbot-nginx
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes upgrade
+sudo apt-get --yes --force-yes install nginx python3 python3-pip software-properties-common
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get --yes --force-yes upgrade update
+sudo apt-get --yes --force-yes upgrade install python-certbot-nginx
 sudo pip3 install tinydb, flask
