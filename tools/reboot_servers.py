aef799ef59f0b11db7f556184a3c25a6d1f40144
Tobias <tobias.d.messner@gmail.com>
2018-04-10 13:35:23 -0600

    More fucking typos

--- tools/reboot_servers.py
+++ tools/reboot_servers.py
@@ -2,8 +2,8 @@ from tinydb import TinyDB
 import subprocess
 def reload():
     config_db = TinyDB('../config.json')
-    subprocess.call('sudo pkill gunicorn')
-    subprocess.call('sudo pkill gunicorn3')
+    subprocess.call('sudo pkill gunicorn', shell=True)
+    subprocess.call('sudo pkill gunicorn3', shell=True)
     for item in config_db.all():
         subprocess.call('sudo gunicorn3 -b 127.0.0.1:%s --chdir /home/tobi/server/%s app:app --daemon'
         % (item['port'], item['folder']) shell=True)
