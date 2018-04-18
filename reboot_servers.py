ce0b039966386a2446e385d3e343b6189fe5d893
root <root@zusor.org>
2018-04-18 02:09:25 +0000

    Fuck

--- reboot_servers.py
+++ reboot_servers.py
@@ -5,9 +5,8 @@ def reload():
     subprocess.call('sudo pkill gunicorn', shell=True)
     subprocess.call('sudo pkill gunicorn3', shell=True)
     for item in config_db.all():
-        subprocess.call('cd /home/tobi/server/%s' % item['folder'], shell=True)
-        subprocess.call('sudo gunicorn3 -b 127.0.0.1:%s %s:app --daemon'
-        % (item['port'], item['app_name']), shell=True)
+        subprocess.call('sudo gunicorn --chdir /home/tobi/server/%s -b 127.0.0.1:%s %s:app --daemon'
+        % (item['folder'], item['port'], item['app_name']), shell=True)
     subprocess.call('systemctl reload nginx.service', shell=True)
 
 if __name__ == "__main__":
